a1581e966b868476a501aa31bb6cccfd41f0b76a
pwszpl <szppszpsz@gmail.com>
2022-06-30 15:48:28 +0200

    Simplify parser jj file by creating mapper in transformObject.java

--- src/main/java/mongo/search/transform/TransformObject.java
+++ src/main/java/mongo/search/transform/TransformObject.java
@@ -1,12 +1,22 @@
 package mongo.search.transform;
 
+import mongo.parser.MongoSearchEngineParserConstants;
 import mongo.parser.Token;
+import mongo.search.util.StringUtil;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class TransformObject {
     private TransformToken functionalToken;
     private List<Object> params;
+
+    public TransformObject(Token token, Object... params){
+        this(token, Arrays.stream(params).collect(Collectors.toList()));
+    }
 
     public TransformObject(Token token, List<Object> params){
         if(token instanceof TransformToken){
@@ -14,7 +24,7 @@ public class TransformObject {
         } else {
             throw new RuntimeException(String.format("Token '%s' is not supported for transform operation.", token.image));
         }
-        this.params = params;
+        this.params = mapList(params);
     }
 
     public List<Object> getParams() {
@@ -24,4 +34,27 @@ public class TransformObject {
     public TransformToken getFunctionalToken() {
         return functionalToken;
     }
+
+    private Object mapObject(Object object) {
+        if(object instanceof Token){
+            Token token = (Token)object;
+            switch(token.kind){
+                case MongoSearchEngineParserConstants.STRING_LITERAL:
+                    return StringUtil.unescape(token).image;
+                case MongoSearchEngineParserConstants.R_NUMBER:
+                    return Double.valueOf(token.image);
+                case MongoSearchEngineParserConstants.OBJ_FIELD:
+                    return token.image;
+            }
+        }
+        if(object instanceof List && ((List)object).get(0).getClass().isAssignableFrom(Token.class)){
+            return mapList((List<Object>) object);
+        }
+        return object;
+    }
+
+    private List<Object> mapList(List<Object> objectList){
+        return objectList.stream().map(this::mapObject).filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
 }
